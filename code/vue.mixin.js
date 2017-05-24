31b4b01f711a9618b775c9cc542ca92c94258702
493549174@qq.com <493549174@qq.com>
2017-05-24 14:26:54 +0800

    添加README描述

--- code/vue.mixin.js
+++
@@ -1,7 +0,0 @@
-initGlobalAPI(Vue) => initUse(Vue) {
-    Vue.mixin = function(mixin) {
-        // 把传入的mixin传入并绑定到整个Vue对象options上， 影响之后所有创建的vue实例
-        this.options = mergeOptions(this.options, mixin);
-        return this
-    };
-}
