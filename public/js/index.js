3ff84b1a34d35c9bd3c085047d2f66500ea29ebe
493549174@qq.com <493549174@qq.com>
2017-06-20 14:31:20 +0800

    抽离mixin

--- public/js/index.js
+++ public/js/index.js
@@ -4,29 +4,13 @@ import VueRouter from 'vue-router';
 import store from './vuex/store.js';
 import routes from './router/route.js';
 import axios from './common/axios.js';
+import mixin from './common/mixin.js';
 import '../css/layout.css';
 import '../css/iconfont/iconfont.css';
 
 Vue.use(VueRouter);
 Vue.use(axios);
-Vue.mixin({
-    changCur: function (target) {
-        return new Promise( (resolve, reject) => {
-            let cur = target.parentNode.getElementsByClassName('cur')[0];
-            if(!!cur) this.removeClass(cur);
-            if(cur == target) {
-                resolve();
-            } else {
-                target.className = 'cur';
-                resolve(true);
-            }
-        });
-    },
-    removeClass: function(target){
-        if(!target) return true;
-        target.className = target.className.replace(/(\s+|^)cur(\s+|$)/, ' ');
-    }
-})
+Vue.mixin(mixin)
 const router = new VueRouter({
     // 默认hash
     // mode: 'history',
